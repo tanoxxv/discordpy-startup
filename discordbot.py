627b646dcb102a1415286810e7c4c46db24eebe9
tanoxxv <61760960+tanoxxv@users.noreply.github.com>
2020-03-04 22:43:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,14 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.event
+async def on_message(message):
+    try:
+        if message.aauthor.bot:
+            return
+        await bot.process_commands(message)
+       except Exception:
+        await message.channe1.send(f'```\n{traceback.format_exc()}\n```')
 
 @client.event
 async def on_message(message):
