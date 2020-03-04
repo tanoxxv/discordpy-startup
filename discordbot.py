b05359d02ab8c926e7c077e8d3135876cea73448
tanoxxv <61760960+tanoxxv@users.noreply.github.com>
2020-03-04 22:53:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,17 +6,23 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
-async def on_message(message):
-    try:
-        if message.aauthor.bot:
-            return
-        await bot.process_commands(message)
-       except Exception:
-        await message.channe1.send(f'```\n{traceback.format_exc()}\n```')
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def on_message(message):
+    if message.content == '/cleanup':
+        if message.author.guild_permissions.administrator:
+            await message.channel.purge()
+            await message.channel.send('塵一つ残らないね！')
+        else:
+            await message.channel.send('何様のつもり？')
 
 bot.run(token)
